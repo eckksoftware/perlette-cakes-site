8d0dc3dbfb7307c5c9c1672ff58faa9d2adb9dae
25Ericcheong <ericcheongkaikit@gmail.com>
2026-05-02 13:42:37 +0800

    task: Add cursive fonts to name of company for styling purposes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  fonts: [{
+    provider: fontProviders.local(),
+    name: "Pencerio",
+    cssVariable: "--font-pencerio",
+    options: {
+      variants: [{
+        src: 
+        [
+            './src/assets/fonts/pencerio/Pencerio-Hairline.woff2',
+            './src/assets/fonts/pencerio/Pencerio-Hairline.woff',
+            './src/assets/fonts/pencerio/Pencerio-Hairline.ttf'
+        ],
+        weight: '50',
+        style: 'normal'
+      }]
+    }
+  }]
+});
